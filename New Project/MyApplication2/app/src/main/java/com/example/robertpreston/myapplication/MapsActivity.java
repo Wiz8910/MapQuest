69606100fed52f4b9b07359db637108eccab20fe
Robert Preston <rbp9bc@mst.edu>
2015-10-14 23:09:49 -0500

    Map Navigation
    Added Button on map to return to main activity, also added framework
    for navigation drawer in map

--- New Project/MyApplication2/app/src/main/java/com/example/robertpreston/myapplication/MapsActivity.java
+++ New Project/MyApplication2/app/src/main/java/com/example/robertpreston/myapplication/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.robertpreston.myapplication;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.location.Criteria;
 import android.location.Location;
@@ -10,10 +12,15 @@ import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -54,7 +61,7 @@ import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONArray;
 
-public class MapsActivity extends FragmentActivity{  // implements View.OnClickListener{
+public class MapsActivity extends FragmentActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks {  // implements View.OnClickListener{
 
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
@@ -71,7 +78,14 @@ public class MapsActivity extends FragmentActivity{  // implements View.OnClickL
     private Timer timer = new Timer();
     //delay time in ms
     private final long DELAY = 4000;
+    private NavigationDrawerFragment mNavigationDrawerFragment;
 
+    //Fragment for current screen
+    private static PlaceholderFragment currentfragment;
+    //Fragment used for map
+    private SupportMapFragment mapfragment;
+
+    private CharSequence mTitle;
     private static String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,13 +95,101 @@ public class MapsActivity extends FragmentActivity{  // implements View.OnClickL
         setUpMapIfNeeded();
         drop_pin = true;
         markers = new ArrayList<Mark>();
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+        mTitle = getTitle();
+//
+//        // Set up the drawer.
+        //TODO: this is the line that causes crash currently
+//        mNavigationDrawerFragment.setUp(
+//                R.id.navigation_drawer,
+//                (DrawerLayout) findViewById(R.id.drawer_layout));
     }
     @Override
     protected void onResume() {
         super.onResume();
         //setUpMapIfNeeded();
     }
+//    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        if (position != 6) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                    .commit();
+        }
+        //start map stuff when its selected have to use
+        if (position == 6) {
+            Intent intent = new Intent(this, MapsActivity.class);
+            //intent.setComponent(new ComponentName("com.example.robertpreston.myapplication", "com.example.robertpreston.myapplication.MapsActivity"));
+            startActivity(intent);
+        }
+    }
 
+    public void onSectionAttached(int number) {
+        switch (number) {
+            case 1:
+                mTitle = getString(R.string.title_section1);
+                break;
+            case 2:
+                mTitle = getString(R.string.title_section2);
+                break;
+            case 3:
+                mTitle = getString(R.string.title_section3);
+                break;
+            case 4:
+                mTitle = getString(R.string.title_section4);
+                break;
+            case 5:
+                mTitle = getString(R.string.title_section5);
+                break;
+            case 6:
+                mTitle = getString(R.string.title_section6);
+                break;
+            case 7:
+                mTitle = getString(R.string.title_section7);
+                break;
+        }
+    }
+
+    public void restoreActionBar() {
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+//        actionBar.setDisplayShowTitleEnabled(true);
+//        actionBar.setTitle(mTitle);
+        Toast.makeText(getApplication().getApplicationContext(),"Restore Action Bar",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+            // Only show items in the action bar relevant to this screen
+            // if the drawer is not showing. Otherwise, let the drawer
+            // decide what to show in the action bar.
+            getMenuInflater().inflate(R.menu.main, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "This will open a settings window", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
@@ -356,6 +458,7 @@ public class MapsActivity extends FragmentActivity{  // implements View.OnClickL
                                 }
                             }
                             //I know this is ugly, this is my workaround for final keyword
+                            //TODO: This is ugly fix it
                             final int target_indx = indx;
 //                            Toast.makeText(getApplicationContext(), "Four", Toast.LENGTH_SHORT).show();
                             text.addTextChangedListener(new TextWatcher() {
@@ -421,10 +524,15 @@ public class MapsActivity extends FragmentActivity{  // implements View.OnClickL
             Toast.makeText(getApplication().getApplicationContext(),"Empty",Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(getApplication().getApplicationContext(), data,Toast.LENGTH_SHORT).show();
-            //Toast.makeText(getApplication().getApplicationContext(),data, Toast.LENGTH_SHORT).show();
-
     }
 
+    public void BackToMain(View v){
+//        Toast.makeText(getApplication().getApplicationContext(),"Back to Main Screen?",Toast.LENGTH_SHORT).show();
+        finish();
+//        Intent intent = new Intent(this, MainActivity.class);
+//        startActivity(intent);
+        //TODO: do we want a fail safe save here before we exit the map screen?
+    }
     public static String makeRequest(String uri, String json) {
         HttpURLConnection urlConnection;
         String url;
@@ -563,6 +671,68 @@ public class MapsActivity extends FragmentActivity{  // implements View.OnClickL
             name = in.readString();
             lat = in.readDouble();
             lon = in.readDouble();
+        }
+    }
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+        private static int fragNumber;
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            fragNumber = sectionNumber;
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            switch(fragNumber)
+            {
+                case 1: //Map
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+                case 2: //Profile
+                    rootView = inflater.inflate(R.layout.profile_activity, container, false);
+                    break;
+                case 3: //Friends
+                    rootView = inflater.inflate(R.layout.friendslist_activity, container, false);
+                    break;
+                case 4: //MeetUps
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+                case 5: //Invites
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+                case 6: //Create Event
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    break;
+                case 7: //App Info
+                    rootView = inflater.inflate(R.layout.map_activity, container, false);
+                    break;
+            }
+            return rootView;
+        }
+
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            ((MainActivity) activity).onSectionAttached(
+                    getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
 }
